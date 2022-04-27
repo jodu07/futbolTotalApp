9230ee556a453246f0d808026836af62f54928c2
jodu07 <jhonnat.dussan@gmail.com>
2022-04-27 01:02:32 -0500

    teamService getTeams

--- src/app/team/services/team.service.ts
+++ src/app/team/services/team.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Team } from '../interfaces/team.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  constructor() { }
+  private apiUrl: string = 'https://apiv2.allsportsapi.com/football/';
+
+  apiKey: string = '55a214ed0bade34db440fab73eff1115bd32a68b47d79d6f855b6c14463c61e7';
+
+  constructor(private http: HttpClient) { }
+
+
+  getTeams(codigo: number): Observable<any>{
+
+    const url = `${this.apiUrl}?&met=Teams&teamId=${codigo}&APIkey=${this.apiKey}`;
+     return this.http.get<Team[]>(url);
+     
+
+  }
+
+ 
 }
