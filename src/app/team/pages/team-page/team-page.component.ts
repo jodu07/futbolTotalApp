a302a659f78884e6cdc5b35c77798236829d30da
jodu07 <jhonnat.dussan@gmail.com>
2022-04-27 01:10:51 -0500

    teamPage buscarTeam and termino

---
+++ src/app/team/pages/team-page/team-page.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Team } from '../../interfaces/team.interface';
+import { TeamService } from '../../services/team.service';
+
+@Component({
+  selector: 'app-team-page',
+  templateUrl: './team-page.component.html',
+  styleUrls: ['./team-page.component.css']
+})
+export class TeamPageComponent implements OnInit {
+
+  
+
+  /*team: Team={
+    team_key:  '',
+    team_name: '',
+    team_logo: '',
+    players: Player={
+    player_key:          0,
+    player_name:         '',
+    player_number:       '',
+    player_country:      null,
+    player_type:         '',
+    player_age:          '',
+    player_match_played: '',
+    player_goals:        '',
+    player_yellow_cards: '',
+    player_red_cards:    '',
+    player_image:        ''
+    },
+    coaches: Coach={
+      coach_name:    '',
+      coach_country: null,
+      coach_age:     null,
+    }
+   };*/
+
+  team: Team[]=[];
+  termino: number = 0;
+
+
+
+  constructor(private _teamService: TeamService) { }
+
+  ngOnInit(): void {    
+  }
+
+  buscar(){
+    
+    console.log(this.termino);
+    this._teamService.getTeams(this.termino)    
+    .subscribe( team =>{
+
+      this.team = team;
+      console.log('este:', this.team);
+
+     
+
+    }           
+
+    );
+      
+
+  }
+
+}
+
--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TeamPageComponent } from './pages/team-page/team-page.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TeamPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    TeamPageComponent
   ]
 })
 export class TeamModule { }
